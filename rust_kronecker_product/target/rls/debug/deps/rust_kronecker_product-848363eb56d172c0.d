771d7d2fc89041d1f187246e6784b8220829be58
Trevor <65122083+TPXS@users.noreply.github.com>
2021-05-06 09:34:18 -0400

    auto

---
+++ rust_kronecker_product/target/rls/debug/deps/rust_kronecker_product-848363eb56d172c0.d
@@ -0,0 +1,5 @@
+/Users/trevorschirra/Documents/GitHub/rosetta-code-contributions/rust_kronecker_product/target/rls/debug/deps/rust_kronecker_product-848363eb56d172c0.rmeta: src/main.rs
+
+/Users/trevorschirra/Documents/GitHub/rosetta-code-contributions/rust_kronecker_product/target/rls/debug/deps/rust_kronecker_product-848363eb56d172c0.d: src/main.rs
+
+src/main.rs:
---
+++ rust_kronecker_product/target/rls/debug/deps/rust_kronecker_product-f1a1944d21dc9326.d
@@ -0,0 +1,5 @@
+/Users/trevorschirra/Documents/GitHub/rosetta-code-contributions/rust_kronecker_product/target/rls/debug/deps/rust_kronecker_product-f1a1944d21dc9326.rmeta: src/main.rs
+
+/Users/trevorschirra/Documents/GitHub/rosetta-code-contributions/rust_kronecker_product/target/rls/debug/deps/rust_kronecker_product-f1a1944d21dc9326.d: src/main.rs
+
+src/main.rs:
